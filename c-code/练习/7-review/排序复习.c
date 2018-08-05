a6d1c187fd110ec4faed32d913c0c2e6efea9841
nash <nashemt@outlook.com>
2018-08-05 20:57:54 +0800

    2018-8-5

--- c-code/练习/7-review/排序复习.c
+++ c-code/练习/7-review/排序复习.c
@@ -11,17 +11,14 @@
 #include <time.h>
 #include <stdarg.h>
 
-if ()
-{
-	int creat_array(int *array[], int count);
-	void des_array(int *array[]);
-	void sort_array(int *array, int count);
-	void travel_fun(int *array, int count, void(*func)(int vary));
-	void tra_func(int *array, int count);
-	void act_fun(int vary);
-	void rand_fun(int *array, int count);
+int creat_array(int **array, int count);
+void des_array(int *array[]);
+void sort_array(int *array, int count);
+void travel_fun(int *array, int count, void(*func)(int vary));
+void tra_func(int *array, int count);
+void act_fun(int vary);
+void rand_fun(int *array, int count);
 
-}
 int main(void)
 {
 	int *array = NULL;
@@ -39,7 +36,6 @@ int main(void)
 	travel_fun(array, count, act_fun);
 	printf("=================\n");
 	sort_array(array, count);
-	//travel_fun(array, count, act_fun);
 	tra_func(array, count);
 	des_array(&array);
 
@@ -48,6 +44,7 @@ int main(void)
 	return 0;
 }
 
+//创建动态数组.
 int creat_array(int **array, int count)
 {
 	int ret = 0;
@@ -72,6 +69,7 @@ END:
 	return 0;
 }
 
+//销毁数组
 void des_array(int *array[])
 {
 	if (array == NULL)
@@ -83,32 +81,70 @@ void des_array(int *array[])
 	}
 }
 
+//排序
 void sort_array(int *array, int count)
 {
-	if ((array == NULL) || (count <= 0))
+	if ((array == NULL))
 	{
 		printf("sort_array input error\n");
 		return;
 	}
-	int *t_array = array;
-	int index = 0;
-
+	
+	if (!(count > 0))//条件判断
+	{
+		return;
+	}
+	/*
+	//冒泡排序-----> ok
+	for (int i = 1; i < count; i++)
+	{
+		for (int j = 0; j < count - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				int temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+			}
+		}
+	}
+	*/
+	/*
+	//选择排序
+	//int *t_array = array;
+	//int index = 0;//index不能放到循环外面, 否则每次循环的初始值不为0!
 	for (int j = count; j > 0; --j)//实际只需要循环count-1次, 但是这里循环了count次
 	{
+		int index = 0;
 		for (int i = 0; i < j; i++)//循环了j次. i最后为j
 		{
-			if (t_array[index] < t_array[i])
+			if (array[index] < array[i])
 			{
 				index = i;
 			}
 		}
-		int temp = t_array[index];
-		t_array[index] = t_array[j - 1];
-		t_array[j - 1] = temp;
+		int temp = array[index];
+		array[index] = array[j - 1];
+		array[j - 1] = temp;
 	}
+	*/
+	//递归方式的选择排序
+	int index = 0;
+	for (int i = 0; i < count; i++)//执行体
+	{
+		if (array[index] < array[i])
+		{
+			index = i;
+		}
+	}
+	int temp = array[index];
+	array[index] = array[count-1];
+	array[count - 1] = temp;
+	sort_array(array, --count);//app
 }
 
-void travel_fun(int *array, int count, void (*func)(int vary))
+//遍历数组1-->回调函数.
+void travel_fun(int *array, int count, void(*func)(int vary))
 {
 	if ((array == NULL) || (count <= 0) || (func == NULL))
 	{
@@ -124,6 +160,7 @@ void travel_fun(int *array, int count, void (*func)(int vary))
 	printf("==================================\n");
 }
 
+//遍历数组2
 void tra_func(int *array, int count)
 {
 	for (int i = 0; i < count; i++)
@@ -132,11 +169,13 @@ void tra_func(int *array, int count)
 	}
 }
 
+//回调动作函数
 void act_fun(int vary)
 {
 	printf("%d\t", vary);
 }
 
+//随机数生成函数.
 void rand_fun(int *array, int count)
 {
 	if ((array == NULL) || (count <= 0))
